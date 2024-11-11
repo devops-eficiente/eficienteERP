3cf924e54b6262405f24708ac2ef34d27b21592d
carlos12huet <carlos777daniel@live.com>
2024-11-11 17:34:00 -0600

    WhatsApp Message

---
+++ app/Livewire/WhatsappMessage.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class WhatsappMessage extends Component
+{
+    public function render()
+    {
+        return view('livewire.whatsapp-message');
+    }
+}
